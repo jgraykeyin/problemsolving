9f48ab0bce787fdd067d612b53b0d6dfaab6edac
jgraykeyin <justin.gray@keyin.com>
2020-10-04 09:19:10 -0230

    HTML output function added

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,10 @@
 # Trying to create a sudoku generator
 import random
+numberIndex = [1,2,3,4,5,6,7,8,9]
+blocker = [[],[],[],[],[],[],[],[],[]]
 
 def createBoard():
-    numberIndex = [1,2,3,4,5,6,7,8,9]
+    global blocker
     board = [[0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],
@@ -14,7 +16,6 @@ def createBoard():
             [0,0,0,0,0,0,0,0,0]
             ]
 
-    blocker = [[],[],[],[],[],[],[],[],[]]
 
     # Setting the amount of numbers to attempt placing into the board, should probably randomize this between a range
     startNumbers = 60
@@ -32,15 +33,11 @@ def createBoard():
     n=0
     x=0
     y=0
-    #add=1
-    #lastNum = 0
-    cellCount=0
     while x <= 9 or len(tryNumbers) > 0:
-        foundVertical = False
         cellSelect = random.randint(0,2)
         y+=cellSelect
 
-        if y >= 8 and x < 8:
+        if y >= 9 and x < 8:
             x+=1
             y=cellSelect
 
@@ -131,7 +128,63 @@ def createBoard():
 
     return(board)
 
+def createHTML(board):
+    web = open("sudoku.html","w")
+    web.write("<!DOCTYPE html><html><head><style>")
+    web.write("table, td {  border: 1px solid black; border-collapse: collapse; } td { padding:20px;}</style></head><body>")
+    web.write("<table>")
+    for row in board:
+        web.write("<tr>")
+        for i in row:
+            if i == 0:
+                web.write("<td></td>")
+            else:
+                web.write("<td>{}</td>".format(i))
+        web.write("</tr>")
+    web.write("</table></body></html>")
+    web.close()
 
+#Create a new sudoku board
 newBoard = createBoard()
 for row in newBoard:
+    print(row)
+
+# Output the puzzle to html
+createHTML(newBoard)
+
+
+# Solve the generated puzzle....
+numList=[]
+x=0
+y=0
+vertList=[]
+horizontalsafe = True
+solveBoard = newBoard.copy()
+while x < 1:
+    
+    if y >= 9 and x < 8:
+        x+=1
+        y=0
+
+    if solveBoard[x][y] == 0:
+        print("do something")
+        numList = numberIndex.copy()
+
+        # Check the horizontal for safety
+        for i in numList:
+            if i in solveBoard[x]:
+                numList.remove(i)
+
+        # Check block list for unsafe numbers
+        for i in numList:
+            if i in blocker[x]:
+                numList.remove(i)
+
+
+    print(numList)
+    # Increment the main counter        
+    y+=1
+
+print("Solving....")
+for row in solveBoard:
     print(row)
